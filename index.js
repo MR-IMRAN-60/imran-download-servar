7c1d9d5109e2e2b4748ba80aad90f601ec913a3a
MR IMRAN <164165136+MR-IMRAN-10@users.noreply.github.com>
2024-04-17 18:25:58 +0000

    imran-downloader

---
+++ index.js
@@ -0,0 +1,13 @@
+const { 
+ttdl, 
+igdl, 
+fbdown, 
+twitter, 
+youtube 
+} = require('./IMRAN/module');
+
+module.exports.ttdl = ttdl
+module.exports.igdl = igdl
+module.exports.fbdown = fbdown
+module.exports.twitter = twitter
+module.exports.youtube = youtube
